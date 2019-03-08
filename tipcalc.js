acec50d1cda007ee60b6f45f1282e9f9ab9927af
brandon-rowe <brandon@cyntax.org>
2019-03-07 22:35:31 -0500

    updates. still not fixed

--- tipcalc.js
+++ tipcalc.js
@@ -2,13 +2,11 @@ function tipCalc()
 	{
    		var tb = document.getElementById('total_bill').value;
     	var tp = document.getElementById('tip_percentage').value;
- 		
- 		percentage = tp / 100;
- 		tip = percentage * tb;
-
+ 		var percentage = tp / 100;
+ 		var tip = percentage * tb;
 
     	var html = 	
-    	'</br>Your total bill is $' + tb.toFixed(2) + 
+    	'</br>Your total bill is $' + tb.toFixed(2);
 		'</br> Your tip is $' + tip.toFixed(2);
 	
     	document.getElementById('tip').innerHTML = html;
