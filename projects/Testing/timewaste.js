76b3ee6a2907ef9cf9b092f72568ce135685bcdc
brandon-rowe <brandon@cyntax.org>
2019-03-12 20:59:01 -0400

    fixed protime

--- projects/Testing/timewaste.js
+++ projects/Testing/timewaste.js
@@ -15,3 +15,22 @@ function wastetime()
     	document.getElementById('waste').innerHTML = html;
 	}
 	document.getElementById('time').addEventListener('click', wastetime);
+
+
+function productivetime() 
+    {
+        var task = document.getElementById('pro_task').value;
+        var minutes = document.getElementById('pro_min').value;
+        var hours = minutes/60
+        var weeklyTime = hours * 7
+        var monthlyTime = hours * 30
+        var yearlyTime = hours * 365
+
+        var html =  
+        '</br>You spend ' + weeklyTime.toFixed(2) + ' hours per week, ' + 
+        monthlyTime.toFixed(2) + ' hours per month, and ' + 
+        yearlyTime.toFixed(2) + ' hours per year on ' + task +'. Good Job!';
+    
+        document.getElementById('productive').innerHTML = html;
+    }
+    document.getElementById('protime').addEventListener('click', productivetime);
