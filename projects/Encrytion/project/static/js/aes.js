e6e7db02dc985c3390823ada0c35d46665b540ea
brandon-rowe <brandon@cyntax.org>
2019-10-14 12:01:30 -0400

    Update html

---
+++ projects/Encrytion/project/static/js/aes.js
@@ -0,0 +1,33 @@
+function aesCipher() 
+	{
+   		var message = document.getElementById('aesInp').value;
+   		var shift = Math.round(Math.random() * 25)
+   		var encryptedMsg = ""
+   		for (var i=0; i<message.length; i++)
+   		{
+    		// Get the character we'll be appending
+    		var c = message[i];
+    			// If it's a letter...
+    		if (c.match(/[a-z]/i)) 
+    			{
+    				// Get its code
+    				var code = message.charCodeAt(i);
+    				// Uppercase letters
+    				if ((code >= 65) && (code <= 90))
+    					c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+    				// Lowercase letters
+    				else if ((code >= 97) && (code <= 122))
+    					c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+    			}
+    		// Append
+    		encryptedMsg += c;
+		}
+
+    	var html = 	
+    	'</br>Your shift value was ' + shift + ' and your original message was, ' + 
+    	message + '. Your encrypted message is ' + 
+    	encryptedMsg;
+	
+    	document.getElementById('aes').innerHTML = html;
+	}
+	document.getElementById('aesEncrypt').addEventListener('click', aesCipher);
