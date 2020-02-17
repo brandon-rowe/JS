c40eeb14b627f22212b4058d2f8c5c3f8c6c1079
brandon-rowe <brandon@cyntax.org>
2020-02-17 11:43:06 -0500

    update dashboard

--- projects/Angular/charts/src/main.ts
+++ projects/Angular/charts/src/main.ts
@@ -1,11 +1,10 @@
 /*
  =========================================================
- * Black Dashboard Angular - v1.0.0
+ * Black Dashboard - v1.0.0
  =========================================================
 
- * Product Page: https://www.creative-tim.com/product/black-dashboard-angular
- * Copyright 2019 Creative Tim (https://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+ * Copyright 2019 Cyntaxm (https://www.cyntax.org)
+ * Licensed under MIT 
 
  =========================================================
 
