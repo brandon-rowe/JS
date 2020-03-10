75557fbd0ac090c9814658651817e7ff319424fc
brandon-rowe <brandon@cyntax.org>
2020-03-09 23:47:35 -0400

    Remove extra folders

--- projects/btc-wallet/js/donate.js
+++
@@ -1,8 +0,0 @@
-
-function donate(){
-    var copytxt = document.getElementById("donate");
-    copytxt.select();
-    copytxt.setSelectionRange(0, 999999);
-    document.execCommand("copy");
-    alert("Copied: " + copytxt.value);
-}
