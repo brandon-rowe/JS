6ad87b42fbd68e4f7c3ef1ee2d655908618a9fec
brandon-rowe <brandon@cyntax.org>
2020-11-30 20:54:03 -0500

    Add updated Encryption project

---
+++ projects/cyntaxEnc/project/static/js/sha256.js
@@ -0,0 +1,32 @@
+function sha256Cipher() 
+	{
+   		var message = document.getElementById('sha256Inp').value;
+   		var shift = Math.round(Math.random() * 25)
+   		var encryptedMsg = ""
+   		for (var i=0; i<message.length; i++)
+   		{
+    		// Get the character we'll be appending
+    		var c = message[i];
+    			// If it's a letter...
+    		if (c.match(/[a-z]/i)) 
+    			{
+    				// Get its code
+    				var code = message.charCodeAt(i);
+    				// Uppercase letters
+    				if ((code >= 65) && (code <= 90))
+    					c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+    				// Lowercase letters
+    				else if ((code >= 97) && (code <= 122))
+    					c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+    			}
+    		// Append
+    		encryptedMsg += c;
+		}
+
+    	var html = 	
+    	'</br> Your encrypted message is ' + 
+    	encryptedMsg;
+	
+    	document.getElementById('sha256').innerHTML = html;
+	}
+	document.getElementById('sha256Encrypt').addEventListener('click', sha256Cipher);
---
+++ projects/cyntaxEnc/project/static/js/sha3.js
@@ -0,0 +1,32 @@
+function sha3Cipher() 
+	{
+   		var message = document.getElementById('sha3Inp').value;
+   		var shift = Math.round(Math.random() * 25)
+   		var encryptedMsg = ""
+   		for (var i=0; i<message.length; i++)
+   		{
+    		// Get the character we'll be appending
+    		var c = message[i];
+    			// If it's a letter...
+    		if (c.match(/[a-z]/i)) 
+    			{
+    				// Get its code
+    				var code = message.charCodeAt(i);
+    				// Uppercase letters
+    				if ((code >= 65) && (code <= 90))
+    					c = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+    				// Lowercase letters
+    				else if ((code >= 97) && (code <= 122))
+    					c = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+    			}
+    		// Append
+    		encryptedMsg += c;
+		}
+
+    	var html = 	
+    	'</br> Your encrypted message is ' + 
+    	encryptedMsg;
+	
+    	document.getElementById('sha3').innerHTML = html;
+	}
+	document.getElementById('sha3Encrypt').addEventListener('click', sha3Cipher);
